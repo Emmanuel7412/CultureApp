dfd8aacc44a25f7d9bd31ccdf82439b54e2b845a
Emmanuel POIRIER <epoirier@idaia.group>
2022-12-05 13:36:38 +0100

    DotNet: create Sqlite db & add users controller

---
+++ ApiBackEnd/CultureApp.Api/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+using ApiBackEnd.CultureApp.Data;
+using ApiBackEnd.CultureApp.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiBackEnd.CultureApp.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            return Ok(user);
+        }
+
+    }
+}
